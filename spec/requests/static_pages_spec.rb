2ece3926df9af3a501faf2573bfb8d0ce0135855
Roman Levin <romanlevin@gmail.com>
2013-06-30 11:52:48 +0000

    User model with validated email and name fields.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -51,7 +51,7 @@ describe "Static pages" do
     expect(page).to have_title(full_title('Contact'))
     click_link "Home"
     click_link "Sign up now!"
-    expect(page).to have_title(full_title('Sign up'))
+    expect(page).to have_title(full_title('Sign Up'))
     click_link "sample app"
     expect(page).to have_title(full_title '')
   end
